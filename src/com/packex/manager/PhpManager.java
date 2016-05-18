bcd2ce59958a14422b61fbcc376c46c10b480828
omaray <omar.develops@gmail.com>
2016-05-18 15:23:44 -0700

    Tranferring PHP and Ruby to also use the Template Pattern for the Language Manager

--- src/com/packex/manager/PhpManager.java
+++ src/com/packex/manager/PhpManager.java
@@ -1,57 +1,34 @@
 package com.packex.manager;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 import com.packex.Constants;
-import com.packex.Util;
-import com.packex.connector.BigQueryConnector;
 import com.packex.loader.PhpLoader;
+import com.packex.loader.LanguageLoader;
 import com.packex.model.pkgmgr.PhpDownloadData;
 
-public class PhpManager implements LanguageManager{
-    private String company;
-    private String packageName;
-    private String category;
-    private String datasetName;
-    private String tableName;
+public class PhpManager extends LanguageManagerBase{
 
     public PhpManager(String company, String packageName, String category) {
-        this.company = company;
-        this.packageName = packageName;
-        this.category = category;
-        this.datasetName = Util.getDatasetName();
-        this.tableName = Util.getTableName(this.company);
+        super(company, packageName, category);
     }
-    
-    public void saveData() {
-        PhpLoader loader = new PhpLoader(this.packageName);
-        loader.loadData();
-        
-        BigQueryConnector connector = BigQueryConnector.getInstance();
-        connector.begin(this.datasetName, this.tableName);
-        
-        Map<String, Object> row = new HashMap<String, Object>();
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        Date dateObj = new Date();
-        
-        // Core fields
-        row.put(Constants.DATE_FIELD, df.format(dateObj) + Constants.NEW_DAY_TIME);
-        row.put(Constants.LANGUAGE_FIELD, Constants.PHP_LANGUAGE);
-        row.put(Constants.PACKAGE_NAME_FIELD, this.packageName);
-        row.put(Constants.VERSION_FIELD, Constants.ALL_VERSIONS);
-        row.put(Constants.CATEGORY_FIELD, this.category);
-        
-        // Download fields
-        PhpDownloadData data = loader.getDownloadData();
+
+    @Override
+    protected LanguageLoader getLanguageLoader() {
+        return new PhpLoader(this.packageName);
+    }
+
+    @Override
+    protected String getLanguageField() {
+        return Constants.PHP_LANGUAGE;
+    }
+
+    @Override
+    protected void putDownloadEntries(LanguageLoader loader, Map<String, Object> row) {
+        PhpDownloadData data = ((PhpLoader)loader).getDownloadData();
         row.put(Constants.TOTAL_DOWNLOADS_FIELD, data.getTotal());
         row.put(Constants.MONTHLY_DOWNLOADS_FIELD, data.getMonthly());
         row.put(Constants.DAILY_DOWNLOADS_FIELD, data.getDaily());
-        
-        connector.addRow(row);
-        connector.commit();
     }
     
     public static void main(String[] args) {
--- src/com/packex/manager/RubyManager.java
+++ src/com/packex/manager/RubyManager.java
@@ -1,63 +1,36 @@
 package com.packex.manager;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 import com.packex.Constants;
-import com.packex.Util;
-import com.packex.connector.BigQueryConnector;
 import com.packex.loader.RubyLoader;
+import com.packex.loader.LanguageLoader;
 import com.packex.model.pkgmgr.RubyDownloadData;
 
-public class RubyManager implements LanguageManager {
-    private String company;
-    private String packageName;
-    private String category;
-    private String datasetName;
-    private String tableName;
+public class RubyManager extends LanguageManagerBase {
     
     public RubyManager(String company, String packageName, String category) {
-        this.company = company;
-        this.packageName = packageName;
-        this.category = category;
-        this.datasetName = Util.getDatasetName();
-        this.tableName = Util.getTableName(this.company);
+        super(company, packageName, category);
     }
-    
-    public void saveData() {
-        RubyLoader loader = new RubyLoader(this.packageName);
-        loader.loadData();
-        
-        BigQueryConnector connector = BigQueryConnector.getInstance();
-        connector.begin(this.datasetName, this.tableName);
-        
-        Map<String, Object> row = new HashMap<String, Object>();
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        Date dateObj = new Date();
-        
-        // Core fields
-        row.put(Constants.DATE_FIELD, df.format(dateObj) + Constants.NEW_DAY_TIME);
-        row.put(Constants.LANGUAGE_FIELD, Constants.RUBY_LANGUAGE);
-        row.put(Constants.PACKAGE_NAME_FIELD, this.packageName);
-        row.put(Constants.VERSION_FIELD, Constants.ALL_VERSIONS);
-        row.put(Constants.CATEGORY_FIELD, this.category);
-        
-        // Download fields
-        RubyDownloadData data = loader.getDownloadData();
+
+    @Override
+    protected LanguageLoader getLanguageLoader() {
+        return new RubyLoader(this.packageName);
+    }
+
+    @Override
+    protected String getLanguageField() {
+        return Constants.RUBY_LANGUAGE;
+    }
+
+    @Override
+    protected void putDownloadEntries(LanguageLoader loader, Map<String, Object> row) {
+        RubyDownloadData data = ((RubyLoader)loader).getDownloadData();
         row.put(Constants.TOTAL_DOWNLOADS_FIELD, data.getDownloads());
-        
-        connector.addRow(row);
-        connector.commit();
     }
     
     public static void main(String[] args) {
         RubyManager rubyManager = new RubyManager("google", "gcloud", "cloud");
         rubyManager.saveData();
-        
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        Date dateobj = new Date();
-        System.out.println(df.format(dateobj));
     }
 }
